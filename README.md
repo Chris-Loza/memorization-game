381ab7e2e0049f8da9b2dfcd56932bf8f031f9e3
Chris-Loza <christianloza99@gmail.com>
2025-01-06 14:20:19 -0800

    first commit, cleaned up vite template

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a memorization game created by me. It is used for the purpose of gaining experience and to add to my personal portfolio.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Memorization Game</div>
+    </>
+  );
+}
+
+export default App;
