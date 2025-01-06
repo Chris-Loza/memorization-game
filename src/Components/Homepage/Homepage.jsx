92359e26ee376adfd8dc672a1ee055b051b7dd7d
Chris-Loza <christianloza99@gmail.com>
2025-01-06 15:16:35 -0800

    added minimal CSS excpet to individual square components

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -5,17 +5,23 @@ const Homepage = () => {
   return (
     <>
       <div className="parentContainer">
-        Homepage
-        <div className="headerContainer">Hire Me</div>
+        <div className="headerContainer">
+          <p>Hire Me</p>
+        </div>
         <div className="gameContainer">
-          Game Container
-					<p><b>Round: </b>#</p>
-					<div className="squareGridContainer">
-						Individual Square Component x9
-					</div>
-					<p><b>Highest Round: </b>#</p>
-				</div>
-        <div className="footerContainer">My Links</div>
+          <p>
+            <b>Round: </b>#
+          </p>
+          <div className="squareGridContainer">
+            Individual Square Component x9
+          </div>
+          <p>
+            <b>Highest Round: </b>#
+          </p>
+        </div>
+        <div className="footerContainer">
+					<div className="myLinks">My Links</div>
+        </div>
       </div>
     </>
   );
